c42f8373ed9c10cc2323f03cd3b38bf2a5f73245
khoatbui <khoatbui.dev@gmail.com>
2019-06-07 16:24:17 +0700

    Them template vao cac trang

--- client/components/HeaderComponent.js
+++ client/components/HeaderComponent.js
@@ -2,7 +2,6 @@
 Vue.component('header-component', {
     data: function () {
       return {
-        searchbox: ""
       }
     },
     template: `<nav class="navbar navbar-color-on-scroll navbar-transparent fixed-top navbar-expand-lg" color-on-scroll="100">
@@ -16,18 +15,100 @@ Vue.component('header-component', {
             <ul class="navbar-nav ml-auto">
                 <li class="nav-item"> <a href="../index.html" class="nav-link"> <i class="fa fa-menu"></i> Home
                     </a> </li>
-                <li class="nav-item"> <a href="../tour/destination.html" class="nav-link"> <i
-                            class="fa fa-menu"></i>
+                <li class="nav-item dropdown">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
                         Destination
-                    </a> </li>
-                <li class="nav-item"> <a href="../tour/travelstyle.html" class="nav-link"> <i
-                            class="fa fa-menu"></i>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <a class="dropdown-item text-info" href="#"><i class="fab fa-buysellads mr-2"></i> VietNam </a>
+                        <a class="dropdown-item text-info" href="#"><i class="fas fa-air-freshener mr-2"></i> Other Country</a>
+                    </div>
+                </li>
+                <li class="nav-item dropdown hestia-mega-menu">
+                    <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                        aria-haspopup="true" aria-expanded="false">
                         Travel Style
-                    </a> </li>
-                <li class="nav-item"> <a href="../tour/travelservice.html" class="nav-link"> <i
-                            class="fa fa-menu"></i>
-                        Travel Service
-                    </a> </li>
+                    </a>
+                    <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                        <div class="row m-1">
+                            <div class="col-7">
+                                <div class="row">
+                                    <div class="col-6 p-1">
+                                        <div class="card bg-dark text-white m-1">
+                                            <a class="nav-card-link" href="../tour/tourlist.html">
+                                                <img class="card-img h-30" src="../img/tour/style_luxury.jpg"
+                                                    alt="Card image">
+                                                <div class="card-img-overlay">
+                                                    <h4 class="card-title text-center">LUXURY</h4>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 p-1">
+                                        <div class="card bg-dark text-white m-1">
+                                            <a class="nav-card-link" href="../tour/tourlist.html">
+                                                <img class="card-img h-30" src="../img/tour/style_golf.jpg"
+                                                    alt="Card image">
+                                                <div class="card-img-overlay">
+                                                    <h4 class="card-title text-center">GOLF</h4>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 p-1">
+                                        <div class="card bg-dark text-white m-1">
+                                            <a class="nav-card-link" href="../tour/tourlist.html">
+                                                <img class="card-img h-30" src="../img/tour/style_classic.jpg"
+                                                    alt="Card image">
+                                                <div class="card-img-overlay">
+                                                    <h4 class="card-title text-center">CLASSIC</h4>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                    <div class="col-6 p-1">
+                                        <div class="card bg-dark text-white m-1">
+                                            <a class="nav-card-link" href="../tour/tourlist.html">
+                                                <img class="card-img h-30" src="../img/tour/style_adventure.jpg"
+                                                    alt="Card image">
+                                                <div class="card-img-overlay">
+                                                    <h4 class="card-title text-center">ADVENTURE</h4>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-5">
+                                <div class="row d-flex align-items-center justify-content-center h-100">
+                                    <div class="col-12 m-1">
+                                        <div class="card bg-dark text-white m-1">
+                                            <a class="nav-card-link" href="../tour/tourlist.html">
+                                                <img class="card-img" src="../img/tour/des_phuquoc.webp"
+                                                    alt="Card image">
+                                                <div class="card-img-overlay">
+                                                    <h4 class="card-title text-center">VIEW ALL</h4>
+                                                </div>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </li>
+                <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" data-toggle="dropdown"
+                            aria-haspopup="true" aria-expanded="false">
+                            Travel Service
+                        </a>
+                        <div class="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
+                            <a class="dropdown-item text-info" href="../hotel/hotel-booking-confirm.html"><i class="fas fa-hotel mr-2"></i> Hotel </a>
+                            <a class="dropdown-item text-info" href="#"><i class="fas fa-taxi mr-2"></i> Car</a>
+                            <a class="dropdown-item text-info" href="#"><i class="fab fa-avianex mr-2"></i> Visa</a>
+                        </div>
+                    </li>
                 <li class="nav-item"> <a href="../tour/mice.html" class="nav-link"> <i class="fa fa-menu"></i> MICE
                     </a> </li>
                 <li class="nav-item"> <a href="../tour/fit.html" class="nav-link"> <i class="fa fa-menu"></i> FIT
@@ -38,7 +119,7 @@ Vue.component('header-component', {
                     </a> </li>
                 <li class="nav-item ">
                     <ul class="navbar-nav">
-                        <li class="active"> <a href="#" class="nav-link"> <img class="language-icon"
+                        <li class="hide"> <a href="#" class="nav-link"> <img class="language-icon"
                                     src="../img/language/vietnam.png" alt=""> | VI
                             </a>
                         </li>
@@ -46,7 +127,7 @@ Vue.component('header-component', {
                                     src="../img/language/united-kingdom.png" alt=""> | EN
                             </a>
                         </li>
-                        <li class="hide"> <a href="#" class="nav-link"> <img class="language-icon"
+                        <li class="active"> <a href="#" class="nav-link"> <img class="language-icon"
                                     src="../img/language/south-korea.png" alt=""> | KO
                             </a>
                         </li>
@@ -67,12 +148,5 @@ Vue.component('header-component', {
     </div>
 </nav>`,
     methods:{
-      toursearch() {
-        if (this.searchbox.length > 0) {
-          window.location = "../tour/tourlist.html?searchbox-search=" + this.searchbox;
-        } else {
-          return;
-        }
-      }
     }
   })
