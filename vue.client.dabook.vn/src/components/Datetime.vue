35a48e352a1ba5704944dd90a336208a85e63abe
Khoatbui <khoatbui.dev@gmail.com>
2019-06-24 15:51:50 +0700

    Style lai datetime

--- vue.client.dabook.vn/src/components/Datetime.vue
+++ vue.client.dabook.vn/src/components/Datetime.vue
@@ -1,18 +1,14 @@
 <template>
-   <HotelDatePicker DatePickerID="01" 
-    :minNights="minNights" 
-    :maxNights="maxNights" 
-    />
+  <HotelDatePicker DatePickerID="01" :minNights="minNights" :maxNights="maxNights"/>
 </template>
 <script>
 import axios from "axios";
-import HotelDatePicker from 'vue-hotel-datepicker'
+import HotelDatePicker from "vue-hotel-datepicker";
 export default {
   components: {
     HotelDatePicker
   },
-  methods: {
-  },
+  methods: {},
   data: function() {
     return {
       minNights: 2,
@@ -22,37 +18,54 @@ export default {
 };
 </script>
 <style lang="scss">
-.datepicker__wrapper{
-  height: 40px;
+.datepicker__wrapper {
+  height: 40px !important;
 }
-.datepicker__dummy-wrapper{
+.datepicker__dummy-wrapper {
   border-radius: 4px;
   height: 40px !important;
 }
-.datepicker__input{
+.datepicker__input {
   height: 40px !important;
 }
-.-hide-on-desktop .datepicker__dummy-wrapper{
+.-hide-on-desktop .datepicker__dummy-wrapper {
   border-radius: 4px;
   overflow: hidden;
 }
-.datepicker__month-day--first-day-selected{
-  border-top-left-radius:1000px !important;
-  border-bottom-left-radius:1000px !important;
-
+.datepicker__month-day--first-day-selected {
+  border-top-left-radius: 1000px !important;
+  border-bottom-left-radius: 1000px !important;
 }
-.datepicker__month-day--last-day-selected{
-  border-top-right-radius:1000px !important;
-  border-bottom-right-radius:1000px !important;
+.datepicker__month-day--last-day-selected {
+  border-top-right-radius: 1000px !important;
+  border-bottom-right-radius: 1000px !important;
 }
-.datepicker__month-day--selected{
-
-}
-.datepicker__month-day--selected:hover{
-  border-top-right-radius:1000px !important;
-  border-bottom-right-radius:1000px !important;
+.datepicker__month-day--selected:hover {
+  border-top-right-radius: 1000px !important;
+  border-bottom-right-radius: 1000px !important;
   background-color: #00ca9d !important;
-      opacity: .25;
-      color:#FFFFFF !important;
+  opacity: 0.25;
+  color: #ffffff !important;
+}
+.datepicker__close-button {
+  top: 20px !important;
+  color: #fff !important;
+}
+.datepicker__dummy-wrapper--no-border.datepicker__dummy-wrapper {
+    border: none;
+}
+.-hide-on-desktop .datepicker__dummy-wrapper {
+  border-top-left-radius: 10px;
+  border-top-right-radius: 10px;
+  background-color: #007bff !important;
+  overflow: hidden;
+    border: none;
+}
+.-hide-on-desktop > .datepicker__dummy-wrapper > .datepicker__input {
+  background: none !important;
+  background-color: #007bff !important;
+  color: #fff !important;
+  margin: 0 !important;
+  padding: 0 !important;
 }
 </style>
--- vue.client.dabook.vn/src/views/Main.vue
+++ vue.client.dabook.vn/src/views/Main.vue
@@ -2,12 +2,12 @@
   <div class="container-fluid main h-100 p-0 m-0 bg-main">
     <div class="row w-100">
       <div class="col-12">
-        <img src="img/main_logo_only.png" class="main-logo m-4" />
+        <img src="img/main_logo_only.png" class="main-logo m-4">
       </div>
     </div>
-    <div class="row main-section p-0 d-flex align-items-start justify-content-center  w-100 m-0">
+    <div class="row main-section p-0 d-flex align-items-start justify-content-center w-100 m-0">
       <div class="col-12">
-        <div class="row p-2 pb-4 ">
+        <div class="row p-2 pb-4">
           <div class="col-12">
             <div class="form-group text-left">
               <label for="iwhere">
@@ -52,82 +52,74 @@
             </h6>
           </div>
           <div class="col-12 scroll-ngang">
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/phuquoc_1.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >Destination</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">Destination</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/phuquoc_2.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >Travel Style</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">Travel Style</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/phuquoc_3.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >Travel Service.</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">Travel Service.</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/quynhon_1.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >MICE.</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">MICE.</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/quynhon_2.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >FIT.</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">FIT.</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/quynhon_3.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >About us.</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">About us.</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/sapa.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >Top Tours.</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">Top Tours.</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
-            <div class="card m-2 d-inline-block">
+            <div class="card m-2 d-inline-block shadow-box">
               <img class="card-img-top" src="img/topdestination/sapa_2.jpg" alt="Card image cap">
-              <div class="card-body">
-                <p
-                  class="card-text"
-                >Top Destination.</p>
+              <div class="card-body text-left">
+                <p class="card-text font-weight-bolder">Top Destination.</p>
+                <p class="card-text">Top destination in the world</p>
               </div>
             </div>
           </div>
         </div>
       </div>
     </div>
-<Navbar></Navbar>
+    <Navbar></Navbar>
   </div>
 </template>
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
 import Datetime from "@/components/Datetime.vue"; // @ is an alias to /src
-import Navbar from "@/components/Navbar.vue"
+import Navbar from "@/components/Navbar.vue";
 
 @Component({
   components: {
@@ -143,27 +135,24 @@ export default class Main extends Vue {}
   height: 100%;
   box-sizing: border-box;
 }
-.scroll-ngang{
+.scroll-ngang {
   width: 100%;
   overflow: hidden;
-  overflow-x: scroll ;
+  overflow-x: scroll;
   white-space: nowrap;
 }
 .scroll-ngang::-webkit-scrollbar {
-width: 0px;
-background: transparent;
+  width: 0px;
+  background: transparent;
 }
-.top-destination .card{
-}
-.top-destination img{
+.top-destination img {
   height: 100px;
-  width: auto;
+  width: 140px;
 }
-.top-destination .card-text{
+.top-destination .card-text {
   white-space: nowrap;
   width: 100px;
   overflow: hidden;
   text-overflow: ellipsis;
 }
 </style>
-
