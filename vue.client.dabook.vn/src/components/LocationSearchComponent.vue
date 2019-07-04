1f3ec234530e2ebb5f6eabfa88453808ad43a0b9
Khoatbui <khoatbui.dev@gmail.com>
2019-07-04 15:23:25 +0700

    Pendding user enter

--- vue.client.dabook.vn/src/components/LocationSearchComponent.vue
+++ vue.client.dabook.vn/src/components/LocationSearchComponent.vue
@@ -1,11 +1,17 @@
 <template>
   <div class="search-component">
     <span class="back-icon" @click="naviBack">
-    <font-awesome-icon class="text-danger" icon="arrow-left"/>
+      <font-awesome-icon class="text-danger" icon="arrow-left" />
     </span>
-    <vue-bootstrap-typeahead v-model="query" :data="data" :placeholder="$t('search_search')" @hit="search" @input="searchEnter($event)"/>
+    <vue-bootstrap-typeahead
+      v-model="query"
+      :data="data"
+      :placeholder="$t('search_search')"
+      @hit="search"
+      @input="searchEnter($event)"
+    />
     <span class="search-icon" @click="search">
-    <font-awesome-icon class="text-danger" icon="search"/>
+      <font-awesome-icon class="text-danger" icon="search" />
     </span>
   </div>
 </template>
@@ -17,158 +23,158 @@ export default {
   components: {
     VueBootstrapTypeahead
   },
-  props: ["id", "radius", "placeholder","searchtype"],
+  props: ["id", "radius", "placeholder", "searchtype"],
   name: "LocationSearchComponent",
   created() {},
   methods: {
     naviBack() {
       this.$router.go(-1);
     },
-    async search(){
-      console.log('Viet');
-      this.$store.commit('showHideLoading',true);
-      var response=[];
-      if (this.searchtype=='ALL') {
-        response=await SearchService.getHotelBySearch(this.query)
+    async search() {
+      this.$store.commit("showHideLoading", true);
+      var response = [];
+      if (this.searchtype == "ALL") {
+        response = await SearchService.getHotelBySearch(this.query);
+      } else if (this.searchtype == "HOTEL") {
+        response = await SearchService.getHotelBySearch(this.query);
+      } else if (this.searchtype == "PROMOTION_HOTEL") {
+        response = await SearchService.getPromotionHotelBySearch(this.query);
+      } else if (this.searchtype == "CAR") {
+        response = await SearchService.getHotelBySearch(this.query);
+      } else if (this.searchtype == "TOUR") {
+        response = await SearchService.getHotelBySearch(this.query);
+      } else if (this.searchtype == "PROMOTION_TOUR") {
+        response = await SearchService.getHotelBySearch(this.query);
+      } else {
+        response = await SearchService.getAllBySearch(this.query);
       }
-      else if (this.searchtype=='HOTEL') {
-        response=await SearchService.getHotelBySearch(this.query)
-      }
-      else if(this.searchtype=='PROMOTION_HOTEL') {
-        response=await SearchService.getPromotionHotelBySearch(this.query)
-      }
-      else if(this.searchtype=='CAR') {
-        response=await SearchService.getHotelBySearch(this.query)
-      }
-      else if(this.searchtype=='TOUR') {
-        response=await SearchService.getHotelBySearch(this.query)
-      }
-      else if(this.searchtype=='PROMOTION_TOUR') {
-        response=await SearchService.getHotelBySearch(this.query)
-      }
-      else{
-        response=await SearchService.getAllBySearch(this.query)
-      }
-              console.log(response);
-      this.$store.commit('asignSearchResult',response.data);
-      this.$root.$emit('userSearchActivity')
-       this.$store.commit('showHideLoading',false);
+      console.log(response);
+      this.$store.commit("asignSearchResult", response.data);
+      this.$root.$emit("userSearchActivity");
+      this.$store.commit("showHideLoading", false);
     },
-    searchEnter(e){
+    searchEnter(e) {
       console.log(e);
-      if (e.key === 13) {
-        search();
-      }
+      var my=this;
+      $(".form-control").keypress(function(event) {
+        var keycode = event.keyCode ? event.keyCode : event.which;
+        if (keycode == "13") {
+          my.search();
+          return;
+        }
+      });
     }
   },
   data: function() {
     return {
       query: "",
-      data:[
-"An Giang",
-"Bà Rịa - Vũng Tàu",
-"Bắc Giang",
-"Bắc Kạn",
-"Bạc Liêu",
-"Bắc Ninh",
-"Bến Tre",
-"Bình Định",
-"Bình Dương",
-"Bình Phước",
-"Bình Thuận",
-"Cà Mau",
-"Cao Bằng",
-"Đắk Lắk",
-"Đắk Nông",
-"Điện Biên",
-"Đồng Nai",
-"Đồng Tháp",
-"Gia Lai",
-"Hà Giang",
-"Hà Nam",
-"Hà Tĩnh",
-"Hải Dương",
-"Hậu Giang",
-"Hòa Bình",
-"Hưng Yên",
-"Khánh Hòa",
-"Kiên Giang",
-"Kon Tum",
-"Lai Châu",
-"Lâm Đồng",
-"Lạng Sơn",
-"Lào Cai",
-"Long An",
-"Nam Định",
-"Nghệ An",
-"Ninh Bình",
-"Ninh Thuận",
-"Phú Thọ",
-"Quảng Bình",
-"Quảng Nam",
-"Quảng Ngãi",
-"Quảng Ninh",
-"Quảng Trị",
-"Sóc Trăng",
-"Sơn La",
-"Tây Ninh",
-"Thái Bình",
-"Thái Nguyên",
-"Thanh Hóa",
-"Thừa Thiên Huế",
-"Tiền Giang",
-"Trà Vinh",
-"Tuyên Quang",
-"Vĩnh Long",
-"Vĩnh Phúc",
-"Yên Bái",
-"Phú Yên",
-"Cần Thơ",
-"Đà Nẵng",
-"Hải Phòng",
-"Hà Nội",
-"TP HCM"
+      data: [
+        "An Giang",
+        "Bà Rịa - Vũng Tàu",
+        "Bắc Giang",
+        "Bắc Kạn",
+        "Bạc Liêu",
+        "Bắc Ninh",
+        "Bến Tre",
+        "Bình Định",
+        "Bình Dương",
+        "Bình Phước",
+        "Bình Thuận",
+        "Cà Mau",
+        "Cao Bằng",
+        "Đắk Lắk",
+        "Đắk Nông",
+        "Điện Biên",
+        "Đồng Nai",
+        "Đồng Tháp",
+        "Gia Lai",
+        "Hà Giang",
+        "Hà Nam",
+        "Hà Tĩnh",
+        "Hải Dương",
+        "Hậu Giang",
+        "Hòa Bình",
+        "Hưng Yên",
+        "Khánh Hòa",
+        "Kiên Giang",
+        "Kon Tum",
+        "Lai Châu",
+        "Lâm Đồng",
+        "Lạng Sơn",
+        "Lào Cai",
+        "Long An",
+        "Nam Định",
+        "Nghệ An",
+        "Ninh Bình",
+        "Ninh Thuận",
+        "Phú Thọ",
+        "Quảng Bình",
+        "Quảng Nam",
+        "Quảng Ngãi",
+        "Quảng Ninh",
+        "Quảng Trị",
+        "Sóc Trăng",
+        "Sơn La",
+        "Tây Ninh",
+        "Thái Bình",
+        "Thái Nguyên",
+        "Thanh Hóa",
+        "Thừa Thiên Huế",
+        "Tiền Giang",
+        "Trà Vinh",
+        "Tuyên Quang",
+        "Vĩnh Long",
+        "Vĩnh Phúc",
+        "Yên Bái",
+        "Phú Yên",
+        "Cần Thơ",
+        "Đà Nẵng",
+        "Hải Phòng",
+        "Hà Nội",
+        "TP HCM"
       ]
     };
   }
 };
 </script>
 <style lang="scss">
-.search-component{
+.search-component {
   position: relative;
 }
-.search-component .back-icon{
+.search-component .back-icon {
   width: 30px;
   height: 30px;
   line-height: 30px;
   border-radius: 100px;
-  background-color: #FFF !important;
+  background-color: #fff !important;
   position: absolute;
   top: 50%;
   left: 2%;
-  transform: translate(0,-50%);
+  transform: translate(0, -50%);
   z-index: 20;
 }
-.search-component .search-icon{
-    width: 30px;
+.search-component .search-icon {
+  width: 30px;
   height: 30px;
   line-height: 30px;
   border-radius: 100px;
-  background-color: #FFF !important;
+  background-color: #fff !important;
   position: absolute;
   top: 50%;
   right: 2%;
-  transform: translate(0,-50%);
+  transform: translate(0, -50%);
   z-index: 20;
 }
 .search-component input {
   border-radius: 100px !important;
   padding: 0 40px;
 }
-.search-component input:focus,.search-component input:hover,.search-component input:active{
+.search-component input:focus,
+.search-component input:hover,
+.search-component input:active {
   box-shadow: none !important;
 }
-.search-component .list-group a{
+.search-component .list-group a {
   text-align: left !important;
 }
 .search input {
