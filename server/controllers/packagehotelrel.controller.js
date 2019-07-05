9860cc6faff09900fe2cc0ba9bbab6587c941d35
Khoatbui <khoatbui.dev@gmail.com>
2019-07-05 08:10:44 +0700

    t

--- server/controllers/packagehotelrel.controller.js
+++ server/controllers/packagehotelrel.controller.js
@@ -118,15 +118,15 @@ module.exports.getmAllPackageBySearch = (req, res, next) => {
     console.log(req.body.keyword);
     PackageHotelREL.find({"isUsed":true}).populate('packageId').populate('hotelId').populate('roomTypeId').populate('supplierId').then(function (pac) {
         var result=pac.filter(item =>{ 
-            return item.hotelId.keyword.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-            item.hotelId.hotelName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-            item.hotelId.hotelCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-            item.packageId.packageName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.packageId.packageCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.supplierId.supplierName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.supplierId.supplierCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.roomTypeId.roomTypeName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.roomTypeId.roomTypeCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1
+            return xoa_dau(item.hotelId.keyword).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.hotelId.hotelName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.hotelId.hotelCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.packageId.packageName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.packageId.packageCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.supplierId.supplierName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.supplierId.supplierCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.roomTypeId.roomTypeName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.roomTypeId.roomTypeCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1
     })
         res.send(result)
     })
@@ -148,15 +148,15 @@ module.exports.getmAllPackageBySearch = (req, res, next) => {
 module.exports.getmAllPromotionPackageBySearch = (req, res, next) => {
     PackageHotelREL.find({"isUsed":true,"isPromote":true}).populate('packageId').populate('hotelId').populate('roomTypeId').populate('supplierId').then(function (pac) {
         var result=pac.filter(item =>{ 
-            return item.hotelId.keyword.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-            item.hotelId.hotelName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-            item.hotelId.hotelCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-            item.packageId.packageName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.packageId.packageCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.supplierId.supplierName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.supplierId.supplierCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.roomTypeId.roomTypeName.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1 ||
-        item.roomTypeId.roomTypeCode.toLowerCase().indexOf(req.body.keyword.toLowerCase())> -1
+            return xoa_dau(item.hotelId.keyword).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.hotelId.hotelName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.hotelId.hotelCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.packageId.packageName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.packageId.packageCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.supplierId.supplierName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.supplierId.supplierCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.roomTypeId.roomTypeName).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1 ||
+            xoa_dau(item.roomTypeId.roomTypeCode).toLowerCase().indexOf(xoa_dau(req.body.keyword).toLowerCase())> -1
     })
         res.send(result)
     })
@@ -165,3 +165,20 @@ module.exports.getmAllPromotionPackageBySearch = (req, res, next) => {
 function checkHotel(age) {
     return item.hotelId.hotelName.toLowerCase().indexOf(req.body.keyword.toLowerCase())
   }
+  function xoa_dau(str) {
+    str = str.replace(/à|á|ạ|ả|ã|â|ầ|ấ|ậ|ẩ|ẫ|ă|ằ|ắ|ặ|ẳ|ẵ/g, "a");
+    str = str.replace(/è|é|ẹ|ẻ|ẽ|ê|ề|ế|ệ|ể|ễ/g, "e");
+    str = str.replace(/ì|í|ị|ỉ|ĩ/g, "i");
+    str = str.replace(/ò|ó|ọ|ỏ|õ|ô|ồ|ố|ộ|ổ|ỗ|ơ|ờ|ớ|ợ|ở|ỡ/g, "o");
+    str = str.replace(/ù|ú|ụ|ủ|ũ|ư|ừ|ứ|ự|ử|ữ/g, "u");
+    str = str.replace(/ỳ|ý|ỵ|ỷ|ỹ/g, "y");
+    str = str.replace(/đ/g, "d");
+    str = str.replace(/À|Á|Ạ|Ả|Ã|Â|Ầ|Ấ|Ậ|Ẩ|Ẫ|Ă|Ằ|Ắ|Ặ|Ẳ|Ẵ/g, "A");
+    str = str.replace(/È|É|Ẹ|Ẻ|Ẽ|Ê|Ề|Ế|Ệ|Ể|Ễ/g, "E");
+    str = str.replace(/Ì|Í|Ị|Ỉ|Ĩ/g, "I");
+    str = str.replace(/Ò|Ó|Ọ|Ỏ|Õ|Ô|Ồ|Ố|Ộ|Ổ|Ỗ|Ơ|Ờ|Ớ|Ợ|Ở|Ỡ/g, "O");
+    str = str.replace(/Ù|Ú|Ụ|Ủ|Ũ|Ư|Ừ|Ứ|Ự|Ử|Ữ/g, "U");
+    str = str.replace(/Ỳ|Ý|Ỵ|Ỷ|Ỹ/g, "Y");
+    str = str.replace(/Đ/g, "D");
+    return str;
+}
