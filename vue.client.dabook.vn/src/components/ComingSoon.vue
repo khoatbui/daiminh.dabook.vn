a952f6f8805ea1fbf01908886c298f5c1f6c3624
Bui Van Khoat <khoatbui.dev@gmail.com>
2019-07-08 22:06:37 +0700

    Chinh sua style comingsoon

--- vue.client.dabook.vn/src/components/ComingSoon.vue
+++ vue.client.dabook.vn/src/components/ComingSoon.vue
@@ -24,13 +24,16 @@
         <span
           class="p-2"
         >Thank you for being patient. We're doing some work on the site and will be back shortly</span>
-        <p class="text-md bg-light text-smd text-primary border-radius-10 p-2 shadow-sm d-flex align-items-center">
+        <p class="text-md bg-light text-smd text-primary border-radius-10 p-2 mb-4 shadow-sm d-flex align-items-center">
           <font-awesome-icon icon="phone" class=" p-1" />19001542
         </p>
-        <hr class="text-white bg-white" style="transform: rotate(150deg);border:1px solid white;width:300%"/>
-        <hr class="text-white bg-white" style="transform: rotate(150deg);border:1px solid white;width:300%"/>
-        <hr class="text-white bg-white" style="transform: rotate(150deg);border:1px solid white;width:300%"/>
-        <hr class="text-white bg-white" style="transform: rotate(150deg);border:1px solid white;width:300%"/>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
+        <span class="text-white bg-white m-2" style="transform: rotate(150deg);border:1px solid white;width:300%"></span>
       </div>
 </template>
 <script>
